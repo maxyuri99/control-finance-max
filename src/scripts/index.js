62f5e49efefe86c9a8a32796a37b8ec013d2edd8
maxyuri99 <maxyurisantos@hotmail.com>
2023-06-12 16:08:54 -0300

    Commit

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,28 @@
-/* Desenvolva sua lógica aqui */
+const handleModal = () => {
+    const modal = document.querySelector('.modal__controller')
+    const registerBtn = document.querySelector('.register__button')
+    
+    registerBtn.addEventListener('click', () => {
+      modal.classList.add('show')
+      modal.showModal()
+            
+    })
+  }
+
+  function setupModalValueInput() {
+    const inputField = document.querySelector('.modal-value__input');
+    const container = document.querySelector('.modal-value-input__container');
+  
+    function handleInputEvent() {
+      if (inputField.validity.valid && inputField.value) {
+        container.classList.add('active');
+      } else {
+        container.classList.remove('active');
+      }
+    }
+  
+    inputField.addEventListener('input', handleInputEvent);
+  }
+  
+  setupModalValueInput();
+  handleModal()
